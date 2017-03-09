5c7c4dcbe1fb2b8ea967f67210fa77f98f2babf3
Haoran Feng <haoran.feng@alo7.com>
2017-03-09 16:49:53 +0800

    works

--- 01/client/reducers/comments.js
+++ 01/client/reducers/comments.js
@@ -1,12 +1,25 @@
-function comments(state = [], action) {
-
+function postComments(state=[], action) {
   switch(action.type) {
-    case 'INCREMENT_LINKS' :
-      console.log('increment');
-
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
     default:
       return state;
   }
+}
+
+function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId] :postComments(state[action.postId], action)
+    }
+  }
+
+  return state;
 }
 
 export default comments;
